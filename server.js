52dd46ae3f3bc91e7493b061b3f974c7e46c53b6
bruno74 <davidsg01997@gmail.com>
2016-12-21 12:54:30 +0100

    Servidor NodeJS

---
+++ server.js
@@ -0,0 +1,19 @@
+// importar
+var express = require('express');
+ 
+// instanciar
+var app = express();
+ 
+// ruteo
+app.get('/', function(req, res){
+  res.sendfile(__dirname + '/public/index.html');
+});
+app.get('/about', function(req, res){
+  //res.sendfile(__dirname + '/public/about.html');
+  res.send(req.acceptsLanguage('es') ? 'yes' : 'no');
+});
+ 
+// escuchar
+app.listen(9000);
+ 
+console.log("Servidor Express escuchando en modo %s", app.settings.env);
